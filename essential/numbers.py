fbad8f6483e7eeb06a84d0df24f4e7b5f340a477
JonathanMendoza <jonathan.m2ndoza@gmail.com>
2023-04-08 20:49:05 -0400

    Examples of types of data in Python

---
+++ essential/numbers.py
@@ -0,0 +1,23 @@
+#Division
+print(3/2)
+
+#Modulo operator
+print (3%2) 
+
+#Precedence of operators
+
+print('*****************Precedence of operators*****************')
+print(20 - 10 / 5 * 3**2) #Left-to-right: () , ** , / , * , -
+
+print((20 - 10) / (5 * 3**2))
+
+#The input() function allows user input
+print('*****************input() functions*****************')
+age = input('Insert your age: ')
+print(type(age))
+newAge = int(age) + 1 
+print(type(newAge))
+print(newAge)
+
+
+
